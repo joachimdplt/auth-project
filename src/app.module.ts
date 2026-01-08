74f5c06a4204341f5a2693b99b149562a76aa262
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-08 18:53:43 +0100

    genereate dir Auth with auth.c .m .s and generate users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
@@ -10,7 +12,9 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal:true,
       envFilePath: '.env',
       cache: true,
-    })
+    }),
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
