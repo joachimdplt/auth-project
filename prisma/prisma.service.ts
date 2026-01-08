6798102b9e60d6037585a983983937e6adcfd55b
joakmannn <165683883+joakmannn@users.noreply.github.com>
2026-01-08 19:17:31 +0100

    create prisma.module to expose PrismaService Globally

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
 import { PrismaClient } from './generated/prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    // ✅ Vérification que DATABASE_URL existe
+    if (!process.env.DATABASE_URL) {
+      throw new Error('DATABASE_URL is not defined in .env file');
+    }
+    const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
     super({ adapter });
-    
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { Prisma } from 'prisma/generated/prisma/browser';
+import { PrismaModule } from 'prisma/prisma.module';
 
 
 @Module({
@@ -14,7 +16,8 @@ import { UsersModule } from './users/users.module';
       cache: true,
     }),
     AuthModule,
-    UsersModule
+    UsersModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
